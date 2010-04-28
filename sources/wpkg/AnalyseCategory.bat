b7a7215ad553bbeb5922f16fba34a852e9fff0ba
olikin <olikin@033478fe-9347-4027-94cf-8a8f83edcaf9>
2010-04-28 21:53:15 +0000

    Classement tout automatique des raccourcis dans All Users

--- sources/wpkg/AnalyseCategory.bat
+++ sources/wpkg/AnalyseCategory.bat
@@ -1,16 +1,25 @@
 :: Ce script permet de réorganiser les menus démarrer de All Users par les installations wpkg, de supprimer les raccourcis lors du remove
+:: Auteur : Olivier Lacroix
+
 @echo off
 if ""=="%Z%" set Z=Y:\unattended\install
+:: mettre DEBUG=1 pour afficher des logs
+SET DEBUG=0
 
 :: regeneration locale de la correspondance ID du package<-> Category
 pushd %Z%\wpkg
-echo Analyse des Category dans packages.xml
-cscript %Z%\wpkg\AnalyseCategory.js
+if "%DEBUG%"=="1" echo Analyse des Category dans packages.xml
+cscript %Z%\wpkg\AnalyseCategory.js > NUL
 Set raccprogstoclass=%systemdrive%\netinst\PackagesCategory.txt
+Set raccpersotag=%systemdrive%\netinst\PackagesCategoryPerso.tag
 
-SET PACKAGE=%1
-SET DOSSIERLNK=%2
-echo # REORGANISATION DU MENU DEMARRER pour le programme %PACKAGE% #
+SET ARGUN=%1
+SET ARGDEUX=%2
+:: patch pour gerer les raccourcis contenant des espaces: on vire les guillemets
+SET PACKAGE=%ARGUN:"=%
+SET DOSSIERLNK=%ARGDEUX:"=%
+
+if "%DEBUG%"=="1" echo # REORGANISATION DU MENU DEMARRER pour le programme %PACKAGE% #
 
 if "%ALLUSERSPROFILE%"=="" Set ALLUSERSPROFILE="C:\Documents and Settings\All Users"
 
@@ -18,39 +27,69 @@ if "%ALLUSERSPROFILE%"=="" Set ALLUSERSPROFILE="C:\Documents and Settings\All Us
 CHCP 1252 > NUL
 	for /F "tokens=2* delims=	" %%a in (c:\tmp.txt) do (
 	CHCP 850 > NUL
-	if exist "%%b" set MenuDemarrer=%%b&& echo Le menu demarrer de AllUsers est dans %%b
+	if exist "%%b" set MenuDemarrer=%%b&& if "%DEBUG%"=="1" echo Le menu demarrer de AllUsers est dans %%b
 )
 
-IF "%3"=="remove" goto remove
-:: pour convertir en minuscule le nom du package fourni
-:: il faudrait faire la meme chose sur l'ID recupere dans le fichier PackagesCategory.txt
-::FOR %%i IN ("A=a" "B=b" "C=c" "D=d" "E=e" "F=f" "G=g" "H=h" "I=i" "J=j" "K=k" "L=l" "M=m" "N=n" "O=o" "P=p" "Q=q" "R=r" "S=s" "T=t" "U=u" "V=v" "W=w" "X=x" "Y=y" "Z=z") DO (
-::	echo %%i
-::	SET "PACKAGE=%%%PACKAGE:%%i%%"
-::	echo %PACKAGE%
-::)
-::echo "Package : %PACKAGE%"
+if exist "%raccpersotag%" del /F "%raccpersotag%" > NUL
+SET raccprogstoclassperso=%Z%\packages\windows\PackagesCategory.txt
 
-echo Rangement de %DOSSIERLNK% par categories dans AllUsers
-for /F "tokens=1,2 delims=;" %%a in (%raccprogstoclass%) do (
+IF "%3"=="remove" goto remove
+
+:: Prise en compte d'un fichier de correspondance pour personnaliser 
+if "%DEBUG%"=="1" echo Examen de %raccprogstoclassperso% pour rangement perso de %DOSSIERLNK% dans AllUsers
+for /F "tokens=1,2 delims=;" %%a in (%raccprogstoclassperso%) do (
 	if "%%a"=="%PACKAGE%" (
-		echo ID: %%a Category: %%b
+		if "%DEBUG%"=="1" echo ID: %%a Category: %%b
 		if exist "%MenuDemarrer%\%DOSSIERLNK%" (
-			echo "%MenuDemarrer%\%DOSSIERLNK%" existe : on le deplace si la category est renseignee.
+			if "%DEBUG%"=="1" echo "%MenuDemarrer%\%DOSSIERLNK%" existe : on le deplace si la category est renseignee.
 			if not "%%a"=="" if not "%%b"=="" (
-				if not exist "%MenuDemarrer%\%%b" echo creation du dossier %%b&& mkdir "%MenuDemarrer%\%%b"
+				if not exist "%MenuDemarrer%\%%b" echo Creation du dossier %%b&& mkdir "%MenuDemarrer%\%%b"
 				if exist "%MenuDemarrer%\%%b\%DOSSIERLNK%" (
 					echo La destination "%MenuDemarrer%\%%b\%DOSSIERLNK%" existe. On la supprime pour remplacement.
 					dir "%MenuDemarrer%\%%b\%DOSSIERLNK%" | findstr /I "<REP>" >NUL
 					if errorlevel 1 (
-						echo Suppression du raccourci %%b\%DOSSIERLNK%
+						if "%DEBUG%"=="1" echo Suppression du raccourci %%b\%DOSSIERLNK%
 						del /F /S "%MenuDemarrer%\%%b\%DOSSIERLNK%"
 					) ELSE (
-						echo Suppression du dossier %%b\%DOSSIERLNK%
+						if "%DEBUG%"=="1" echo Suppression du dossier %%b\%DOSSIERLNK%
 						rd /S /Q "%MenuDemarrer%\%%b\%DOSSIERLNK%"
 					)
 				)
-				echo Rangement du fichier-dossier %DOSSIERLNK% de l'appli %%a vers la categorie %%b
+				echo Rangement personnalise du fichier-dossier %DOSSIERLNK% de l'application %%a vers la categorie %%b
+				move /Y "%MenuDemarrer%\%DOSSIERLNK%" "%MenuDemarrer%\%%b\"
+				echo OK > "%raccpersotag%"
+			)
+		) ELSE (
+			echo "%MenuDemarrer%\%DOSSIERLNK%" n existe pas. Bizarre.
+		)
+	)
+)
+
+:: On teste si le raccourci a deja ete classe a l'aide du fichier personnalise. Si oui, on s'arrete la.
+if exist "%raccpersotag%" goto END
+if "%DEBUG%"=="1" echo Action par defaut : aucune correspondance n'a ete trouvee dans %raccprogstoclassperso%
+
+:: Debut de l'action officielle : classement dans le nom de la category par defaut
+if "%DEBUG%"=="1" echo Rangement de %DOSSIERLNK% par categories dans AllUsers
+for /F "tokens=1,2 delims=;" %%a in (%raccprogstoclass%) do (
+	if "%%a"=="%PACKAGE%" (
+		if "%DEBUG%"=="1" echo ID: %%a Category: %%b
+		if exist "%MenuDemarrer%\%DOSSIERLNK%" (
+			if "%DEBUG%"=="1" echo "%MenuDemarrer%\%DOSSIERLNK%" existe : on le deplace si la category est renseignee.
+			if not "%%a"=="" if not "%%b"=="" (
+				if not exist "%MenuDemarrer%\%%b" echo Creation du dossier %%b&& mkdir "%MenuDemarrer%\%%b"
+				if exist "%MenuDemarrer%\%%b\%DOSSIERLNK%" (
+					echo La destination "%MenuDemarrer%\%%b\%DOSSIERLNK%" existe. On la supprime pour remplacement.
+					dir "%MenuDemarrer%\%%b\%DOSSIERLNK%" | findstr /I "<REP>" >NUL
+					if errorlevel 1 (
+						if "%DEBUG%"=="1" echo Suppression du raccourci %%b\%DOSSIERLNK%
+						del /F /S "%MenuDemarrer%\%%b\%DOSSIERLNK%"
+					) ELSE (
+						if "%DEBUG%"=="1" echo Suppression du dossier %%b\%DOSSIERLNK%
+						rd /S /Q "%MenuDemarrer%\%%b\%DOSSIERLNK%"
+					)
+				)
+				echo Rangement du fichier-dossier %DOSSIERLNK% de l'application %%a vers la categorie %%b
 				move /Y "%MenuDemarrer%\%DOSSIERLNK%" "%MenuDemarrer%\%%b\"
 			)
 		) ELSE (
@@ -62,22 +101,46 @@ for /F "tokens=1,2 delims=;" %%a in (%raccprogstoclass%) do (
 goto END
 
 :remove
-echo Suppression de %DOSSIERLNK% du package %PACKAGE%
-for /F "tokens=1,2 delims=;" %%a in (%raccprogstoclass%) do (
+:: Classement perso par Category
+if "%DEBUG%"=="1" echo Suppression automatique de %DOSSIERLNK% du package %PACKAGE% : rangement personnalise. Examen de %raccprogstoclassperso%
+for /F "tokens=1,2 delims=;" %%a in (%raccprogstoclassperso%) do (
 	if "%%a"=="%PACKAGE%" (
-		echo ID: %%a Category: %%b
+		if "%DEBUG%"=="1" echo ID: %%a Category: %%b
 		if exist "%MenuDemarrer%\%%b\%DOSSIERLNK%" (
 			echo "%MenuDemarrer%\%%b\%DOSSIERLNK%" existe : on le supprime.
 			if not "%%a"=="" if not "%%b"=="" (
 				dir "%MenuDemarrer%\%%b\%DOSSIERLNK%" | findstr /I "<REP>" >NUL
 				if errorlevel 1 (
-					echo Suppression du raccourci obsolete %%b\%DOSSIERLNK%
+					if "%DEBUG%"=="1" echo Suppression du raccourci obsolete %%b\%DOSSIERLNK%
 					del /F /S "%MenuDemarrer%\%%b\%DOSSIERLNK%"
 				) ELSE (
-					echo Suppression du dossier obsolete %%b\%DOSSIERLNK%
+					if "%DEBUG%"=="1" echo Suppression du dossier obsolete %%b\%DOSSIERLNK%
 					rd /S /Q "%MenuDemarrer%\%%b\%DOSSIERLNK%"
 				)
-				echo Suppression de la categorie "%MenuDemarrer%\%%b" si vide.
+				if "%DEBUG%"=="1" echo Suppression de la categorie "%MenuDemarrer%\%%b" si vide.
+				dir /B "%MenuDemarrer%\%%b" | find /V "" >NUL || rd /S /Q "%MenuDemarrer%\%%b"
+			)
+		)
+	)
+)
+
+:: Classement officiel par Category
+if "%DEBUG%"=="1" echo Suppression automatique de %DOSSIERLNK% du package %PACKAGE% : rangement officiel. Examen de %raccprogstoclass%
+for /F "tokens=1,2 delims=;" %%a in (%raccprogstoclass%) do (
+	if "%%a"=="%PACKAGE%" (
+		if "%DEBUG%"=="1" echo ID: %%a Category: %%b
+		if exist "%MenuDemarrer%\%%b\%DOSSIERLNK%" (
+			echo "%MenuDemarrer%\%%b\%DOSSIERLNK%" existe : on le supprime.
+			if not "%%a"=="" if not "%%b"=="" (
+				dir "%MenuDemarrer%\%%b\%DOSSIERLNK%" | findstr /I "<REP>" >NUL
+				if errorlevel 1 (
+					if "%DEBUG%"=="1" echo Suppression du raccourci obsolete %%b\%DOSSIERLNK%
+					del /F /S "%MenuDemarrer%\%%b\%DOSSIERLNK%"
+				) ELSE (
+					if "%DEBUG%"=="1" echo Suppression du dossier obsolete %%b\%DOSSIERLNK%
+					rd /S /Q "%MenuDemarrer%\%%b\%DOSSIERLNK%"
+				)
+				if "%DEBUG%"=="1" echo Suppression de la categorie "%MenuDemarrer%\%%b" si vide.
 				dir /B "%MenuDemarrer%\%%b" | find /V "" >NUL || rd /S /Q "%MenuDemarrer%\%%b"
 			)
 		)
