7883f21c8baa7147e6e5e09b1504968f52851b49
mollef <franck.molle@ac-rouen.fr>
2016-02-18 18:10:05 +0100

    version git du script de build

---
+++ build/build.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+arg1="$1"
+
+script_dir=$(cd $(dirname "$0"); pwd)
+pkg_name="se3-wpkg"
+
+cd "$script_dir" || {
+    echo "Error, impossible to change directory to $script_dir."
+    echo "End of the script."
+    exit 1
+}
+
+# Remove old *.deb files.
+rm -rf "$script_dir/"*.deb
+
+cp -ra "$script_dir/../sources" "$script_dir/$pkg_name"
+
+if [ "$arg1" = "update-version" ]
+then
+    # Update the version number.
+    commit_id=$(git log --format="%H" -n 1 | sed -r 's/^(.{10}).*$/\1/')
+    epoch=$(date '+%s')
+    # It's better to prefix by "0." to have a version number
+    # lower than the version numbers of the stable and
+    # official releases.
+    version="0.${epoch}~${commit_id}"
+    sed -i -r "s/^Version:.*$/Version: ${version}/" "$script_dir/se3-wpkg/DEBIAN/control"
+fi
+
+cd  "$script_dir/$pkg_name"
+dh_clean
+debuild -uc -us -b
+# Cleaning.
+rm -r "$script_dir/$pkg_name"
+
+
