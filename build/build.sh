1c1f05a0a69bdfd0dc2ccf5ebf9b6849ced47feb
mollef <franck.molle@ac-rouen.fr>
2016-03-25 01:03:51 +0100

    suppression code inutile

--- build/build.sh
+++ build/build.sh
@@ -16,17 +16,6 @@ rm -rf "$script_dir/"*.deb
 
 cp -ra "$script_dir/../sources" "$script_dir/$pkg_name"
 
-if [ "$arg1" = "update-version" ]
-then
-    # Update the version number.
-    commit_id=$(git log --format="%H" -n 1 | sed -r 's/^(.{10}).*$/\1/')
-    epoch=$(date '+%s')
-    # It's better to prefix by "0." to have a version number
-    # lower than the version numbers of the stable and
-    # official releases.
-    version="0.${epoch}~${commit_id}"
-    sed -i -r "s/^Version:.*$/Version: ${version}/" "$script_dir/se3-wpkg/DEBIAN/control"
-fi
 
 cd  "$script_dir/$pkg_name"
 find ./ \( -name *.sh -o -name *.pl -o -name *.py \) -exec chmod +x {} \;
