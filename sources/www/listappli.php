b396dfaab75d89bc9d61cb08624e0c302959d9bc
lolo4992 <laurentjoly@orange.fr>
2017-06-14 01:19:10 +0200

    Version 1 de la page liste appli

--- sources/www/listappli.php
+++ sources/www/listappli.php
@@ -2,58 +2,48 @@
 <HEAD>
 <meta charset="utf-8">
 <link rel="StyleSheet" type="text/css" href="style.css"></HEAD>
-<title>Version des paquets SE3 wheezy</title>
+<title>Version des applications du serveur</title>
 <meta name=generator content=HTML::TextToHTML v2.51/>
 </head>
-<body background="fond_SE3.png">
-<h1>Liste des applications déployables sur SE3 wheezy</h1>
+<body background="../elements/images/fond_SE3.png">
+<h1>Liste des applications déployables sur votre SE3</h1>
 <?php
 	
-	$wawadeburl="wpkg-list-ng";
 	$svnurl="http://svn.tice.ac-caen.fr/svn/SambaEdu3/wpkg-packages-ng";
 	
-	$xml_stable = simplexml_load_file($wawadeburl."/packages_stable.xml");
-	$xml_testing = simplexml_load_file($wawadeburl."/packages_testing.xml");
+	$xml_packages = simplexml_load_file("/var/se3/unattended/install/wpkg/packages.xml");
+	$xml_profiles = simplexml_load_file("/var/se3/unattended/install/wpkg/profiles.xml");
 	
-	$liste=array();
+	$liste_appli=array();
+	$liste_profiles=array();
+	$liste_profiles2=array();
 	
-	foreach ($xml_stable->package as $app)
+	foreach ($xml_packages->package as $app)
 	{
-
-		$liste[] = array("id"=>$app["id"],
-						 "xml"=>$app["xml"],
-						 "url"=>$app["url"],
-						 "md5sum"=>$app["md5sum"],
-						 "date"=>$app["date"],
-						 "svn_link"=>$app["svn_link"],
+		if ($app["category2"]!="")
+			$app["category"]=$app["category2"];
+		else
+			$app["category"]=$app["category"]."*";
+		$liste_appli[] = array("id"=>$app["id"],
 						 "category"=>str_replace("'"," ",$app["category"]),
 						 "name"=>str_replace("'"," ",$app["name"]),
 						 "compatibilite"=>$app["compatibilite"],
-						 "revision"=>$app["revision"],
-						 "branche"=>"stable");
-	}
-	foreach ($xml_testing->package as $app)
-	{
-
-		$liste[] = array("id"=>$app["id"],
-						 "xml"=>$app["xml"],
-						 "url"=>$app["url"],
-						 "md5sum"=>$app["md5sum"],
-						 "date"=>$app["date"],
-						 "svn_link"=>$app["svn_link"],
-						 "category"=>str_replace("'"," ",$app["category"]),
-						 "name"=>str_replace("'"," ",$app["name"]),
-						 "compatibilite"=>$app["compatibilite"],
-						 "revision"=>$app["revision"],
-						 "branche"=>"testing");
+						 "revision"=>$app["revision"]);
 	}
 	
-	foreach ($liste as $key => $row)
+	foreach ($xml_profiles->profile as $profile1)
+	{
+		foreach ($profile1->package as $profile2)
+			$liste_profiles[(string) $profile2["package-id"]][] = (string) $profile1["id"];
+		foreach ($profile1->depends as $profile2)
+			$liste_profiles2[(string) $profile2["profile-id"]][] = (string) $profile1["id"];
+	}
+	
+	foreach ($liste_appli as $key => $row)
 	{
 		$name[$key]  = strtolower($row['name']);
 		$category[$key] = $row['category'];
-		$date[$key] = substr($row['date'],0,4).substr($row['date'],5,2).substr($row['date'],8,2).substr($row['date'],11,2).substr($row['date'],14,2).substr($row['date'],17,2);
-		$branche[$key] = $row['branche'];
+		$compatibilite[$key] = $row['compatibilite']+0;
 		$revision[$key] = $row['revision'];
 	}
 	
@@ -65,59 +55,81 @@
 	switch ($tri)
 	{
 		case 0:
-		array_multisort($name, SORT_ASC, $branche, SORT_ASC, $liste);
+		array_multisort($name, SORT_ASC, $liste_appli);
 		break;
 		case 1:
-		array_multisort($category, SORT_ASC, $branche, SORT_ASC, $liste);
+		array_multisort($category, SORT_ASC, $name, SORT_ASC, $liste_appli);
 		break;
 		case 2:
-		array_multisort($date, SORT_DESC, $branche, SORT_ASC, $liste);
+		array_multisort($compatibilite, SORT_DESC, $name, SORT_ASC, $liste_appli);
+		break;
+		case 3:
+		array_multisort($name, SORT_DESC, $liste_appli);
+		break;
+		case 4:
+		array_multisort($category, SORT_DESC, $name, SORT_ASC, $liste_appli);
+		break;
+		case 5:
+		array_multisort($compatibilite, SORT_ASC, $name, SORT_ASC, $liste_appli);
 		break;
 		default:
-		array_multisort($name, SORT_ASC, $branche, SORT_ASC, $liste);
+		array_multisort($name, SORT_ASC, $branche, SORT_ASC, $liste_appli);
 		break;
 	}
 
 	
 	echo "<table cellspadding='2' cellspacing='1' border='0' align='center' bgcolor='black'>";
 	echo "<tr bgcolor='white' height='30' valing='center'>";
-	echo "<th width='300'><a href='?tri=0'>Nom de l'application</a></th>";
+	echo "<th width='300'><a href='?tri=";
+	if ($tri==0)
+		echo "3";
+	else
+		echo "0";
+	echo "'>Nom de l'application</a></th>";
 	echo "<th width='120'>Version</th>";
-	echo "<th width='120'>Compatibilit&#233;</th>";
-	echo "<th width='150'><a href='?tri=1'>Cat&#233;gorie</a></th>";
-	echo "<th width='50'>Info</th>";
-	echo "<th width='80'>branche</th>";
-	echo "<th width='120'><a href='?tri=2'>Mise &#224; jour</a></th>";
+	echo "<th width='120'><a href='?tri=";
+	if ($tri==2)
+		echo "5";
+	else
+		echo "2";
+	echo "'>Compatibilit&#233;</a></th>";
+	echo "<th width='150'><a href='?tri=";
+	if ($tri==1)
+		echo "4";
+	else
+		echo "1";
+	echo "'>Cat&#233;gorie</a></th>";
+	echo "<th width='120'>Liste des parcs</th>";
 	echo "</tr>";
-	foreach ($liste as $application)
+	foreach ($liste_appli as $application)
 	{
 		echo "<tr bgcolor='white' height='30' valing='center'>";
-		echo "<td><a href='".$application["url"]."' target='info'>".$application["name"]."</a></td>";
+		echo "<td><a href='index.php?extractAppli=".$application["id"]."' target='info'>".$application["name"]."</a></td>";
 		echo "<td align='center'>".$application["revision"]."</td>";
 		echo "<td align='center'>";
 		
 		switch ($application["compatibilite"])
 		{
 			case 1:
-			echo "<img src='".$wawadeburl."/winxp.png' witdh='20' height='20'>";
+			echo "<img src='winxp.png' witdh='20' height='20'>";
 			break;
 			case 2:
-			echo "<img src='".$wawadeburl."/win7.png' witdh='20' height='20'>";
+			echo "<img src='win7.png' witdh='20' height='20'>";
 			break;
 			case 3:
-			echo "<img src='".$wawadeburl."/winxp.png' witdh='20' height='20'><img src='".$wawadeburl."/win7.png' witdh='20' height='20'>";
+			echo "<img src='winxp.png' witdh='20' height='20'><img src='win7.png' witdh='20' height='20'>";
 			break;
 			case 4:
-			echo "<img src='".$wawadeburl."/win10.png' witdh='20' height='20'>";
+			echo "<img src='win10.png' witdh='20' height='20'>";
 			break;
 			case 5:
-			echo "<img src='".$wawadeburl."/winxp.png' witdh='20' height='20'><img src='".$wawadeburl."/win10.png' witdh='20' height='20'>";
+			echo "<img src='winxp.png' witdh='20' height='20'><img src='win10.png' witdh='20' height='20'>";
 			break;
 			case 6:
-			echo "<img src='".$wawadeburl."/win7.png' witdh='20' height='20'><img src='".$wawadeburl."/win10.png' witdh='20' height='20'>";
+			echo "<img src='win7.png' witdh='20' height='20'><img src='win10.png' witdh='20' height='20'>";
 			break;
 			case 7:
-			echo "<img src='".$wawadeburl."/winxp.png' witdh='20' height='20'><img src='".$wawadeburl."/win7.png' witdh='20' height='20'><img src='".$wawadeburl."/win10.png' witdh='20' height='20'>";
+			echo "<img src='winxp.png' witdh='20' height='20'><img src='win7.png' witdh='20' height='20'><img src='win10.png' witdh='20' height='20'>";
 			break;
 			case 0:
 			echo "";
@@ -129,14 +141,21 @@
 		}
 		echo "</td>";
 		echo "<td align='center'>".$application["category"]."</td>";
-		echo "<td align='center'><a href='".$application["svn_link"]."' target='info'>Info</a></td>";
-		echo "<td align='center'>".$application["branche"]."</td>";
-		echo "<td align='center'>".substr($application["date"],8,2)."/".substr($application["date"],5,2)."/".substr($application["date"],0,4)."</td>";
+		echo "<td align='center'>";
+		if (isset($liste_profiles[(string) $application["id"]]))
+		{
+			$i=0;
+			foreach ($liste_profiles[(string) $application["id"]] as $toto)
+			{
+				if ($i>0)
+					echo ", ";
+				else
+					$i++;
+				echo $toto;
+			}
+		}
+		echo "</td>";
 		echo "</tr>";
-		/*echo $application["id"]." ";
-		echo $application["md5sum"]." ";
-		echo $application["xml"]." ";
-		*/
 
 	}
 	echo "</table>";
