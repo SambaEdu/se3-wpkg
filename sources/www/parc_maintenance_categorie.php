9bdde7572e0312c801165ca0756f557fa465bee3
lolo4992 <laurentjoly@orange.fr>
2018-02-04 18:31:46 +0100

    page catogérie pour parc
    - ajout d'une page vierge

---
+++ sources/www/parc_maintenance_categorie.php
@@ -0,0 +1,33 @@
+<?php
+
+	if (isset($_GET["tri"]))
+		$tri=$purifier->purify($_GET["tri"])+0;
+	else
+		$tri=0;
+	if (isset($_GET["tri2"]))
+		$tri2=$purifier->purify($_GET["tri2"])+0;
+	else
+		$tri2=0;	
+	if (isset($_GET['Appli']))
+		$get_Appli=$purifier->purify($_GET['Appli']);
+	else
+		$get_Appli="";
+	if (isset($_GET['parc']))
+		$get_parc=$purifier->purify($_GET['parc']);
+	else
+		$get_parc="_TousLesPostes";
+	if (isset($_GET["warning"]))
+		$get_warning=$purifier->purify($_GET["warning"])+0;
+	else
+		$get_warning=1;
+	if (isset($_GET["error"]))
+		$get_error=$purifier->purify($_GET["error"])+0;
+	else
+		$get_error=1;
+	if (isset($_GET["ok"]))
+		$get_ok=$purifier->purify($_GET["ok"])+0;
+	else
+		$get_ok=1;
+	
+	header ('Location: parc_maintenance.php?tri2=".$tri2."&parc=".$get_parc."&ok=".$get_ok."&warning=".$get_warning."&error=".$get_error);
+?>
