7e9d0b04f659c1f59f1e6cc9413b540d2ea98941
lolo4992 <laurentjoly@orange.fr>
2018-02-04 18:45:08 +0100

    correction synthaxique

--- sources/www/parc_maintenance_categorie.php
+++ sources/www/parc_maintenance_categorie.php
@@ -1,5 +1,29 @@
 <?php
 
+// loading libs and init
+	include "entete.inc.php";
+	include "ldap.inc.php";
+	include "ihm.inc.php";
+	include "wpkg_lib.php";
+	
+	$login = isauth();
+	if (! $login )
+	{
+		echo "<script language=\"JavaScript\" type=\"text/javascript\">\n<!--\n";
+		$request = '/wpkg/index.php';
+		echo "top.location.href = '/auth.php?request=" . rawurlencode($request) . "';\n";
+		echo "//-->\n</script>\n";
+		exit;
+	}
+	
+	if (is_admin("computers_is_admin",$login)!="Y")
+		die (gettext("Vous n'avez pas les droits suffisants pour acc&#233;der &#224; cette fonction")."</BODY></HTML>");
+
+	// HTMLpurifier
+	include("../se3/includes/library/HTMLPurifier.auto.php");
+	$config = HTMLPurifier_Config::createDefault();
+	$purifier = new HTMLPurifier($config);
+	
 	if (isset($_GET["tri"]))
 		$tri=$purifier->purify($_GET["tri"])+0;
 	else
