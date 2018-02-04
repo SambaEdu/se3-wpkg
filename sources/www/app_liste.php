f6fdc582708da06627d81cfe662101bde6b45e06
lolo4992 <laurentjoly@orange.fr>
2018-02-04 20:14:53 +0100

    nouvelle présentation de la liste des applications

--- sources/www/app_liste.php
+++ sources/www/app_liste.php
@@ -34,12 +34,53 @@
 	include("../se3/includes/library/HTMLPurifier.auto.php");
 	$config = HTMLPurifier_Config::createDefault();
 	$purifier = new HTMLPurifier($config);
+
 	if (isset($_GET["tri"]))
 		$tri=$purifier->purify($_GET["tri"])+0;
 	else
 		$tri=0;
+	if (isset($_GET["tri2"]))
+		$tri2=$purifier->purify($_GET["tri2"])+0;
+	else
+		$tri2=0;	
+	if (isset($_GET['Appli']))
+		$get_Appli=$purifier->purify($_GET['Appli']);
+	else
+		$get_Appli="";
+	if (isset($_GET['parc']))
+		$get_parc=$purifier->purify($_GET['parc']);
+	else
+		$get_parc="";
+	if (isset($_GET["warning"]))
+		$get_warning=$purifier->purify($_GET["warning"])+0;
+	else
+		$get_warning=1;
+	if (isset($_GET["error"]))
+		$get_error=$purifier->purify($_GET["error"])+0;
+	else
+		$get_error=1;
+	if (isset($_GET["ok"]))
+		$get_ok=$purifier->purify($_GET["ok"])+0;
+	else
+		$get_ok=0;
+	if (isset($_GET["tous"]))
+		$get_tous=$purifier->purify($_GET["tous"])+0;
+	else
+		$get_tous=0;
 
-	echo "<h1>Liste des applications du serveur</h1>";
+	
+	
+	echo "<form method='get' action=''>\n";
+	$page_id=0;
+	include ("app_top.php");
+	
+	echo "<input type='hidden' name='parc' value='".$get_parc."'>";
+	echo "<input type='hidden' name='tous' value='".$get_tous."'>";
+	echo "<input type='hidden' name='ok' value='".$get_ok."'>";
+	echo "<input type='hidden' name='warning' value='".$get_warning."'>";
+	echo "<input type='hidden' name='error' value='".$get_error."'>";
+	echo "<input type='hidden' name='tri2' value='".$tri2."'>";
+	echo "</form>\n";
 		
 	$liste_appli=get_list_wpkg_app($xml_packages, $xml_time);
 	$liste_appli_postes=get_list_wpkg_poste_app_all($xml_profiles, $xml_packages);
